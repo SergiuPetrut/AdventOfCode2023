773b76a6ef16e8104b1d9dc0477fd31fc90a9023
D3SXX <9647752d@gmail.com>
2023-12-03 22:39:53 +0200

    Add comments

--- day1/Python/Ex1.py
+++ day1/Python/Ex1.py
@@ -1,24 +1,23 @@
-file = open("ex1-file.txt","r")
-endoutput = 0
-Lines = file.readlines()
-for line in Lines:
+file = open("ex1-file.txt","r") # Open file in read-mode
+endoutput = 0 # Variable for the final sum
+Lines = file.readlines() # Read file by lines
+for line in Lines: # Going thru each line
     try:
-        endResult = ""
-        start = 0
-        end = len(line) - 1
-        i = 0
+        endResult = "" # Variable for holding the first digit and the last digit
+        end = len(line) - 1 # Get the size of string (line)
+        i = 0 # Start from left to right (for the first digit)
         while i < end:
             if(line[i].isnumeric()):
                 endResult += line[i]
                 break
             i += 1
-        i = end
+        i = end # Start for right to left (for the last digit)
         while i >= 0:
             if(line[i].isnumeric()):
                 endResult += line[i]
                 break
             i -= 1
-        endoutput += int(endResult)
+        endoutput += int(endResult) # Add result to the final sum
     except Exception as e:
         pass
-print(endoutput)
+print(f"Result: {endoutput}") # Print result
--- day1/Python/Ex2.py
+++ day1/Python/Ex2.py
@@ -1,27 +1,26 @@
-digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-digitsNumbers = [1,2,3,4,5,6,7,8,9]
-file = open("ex1-file.txt","r")
-endoutput = 0
-Lines = file.readlines()
-for line in Lines:
+digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"] # List for holding pre-defined strings
+digitsNumbers = [1,2,3,4,5,6,7,8,9] # Representation of each string in digits
+file = open("ex1-file.txt","r") # Open file in read-mode
+endoutput = 0 # Variable for the final sum
+Lines = file.readlines() # Read file by lines
+for line in Lines: # Read thru each line
     try:
-        endResult = 0
-        start = 0
-        end = len(line) - 1
+        endResult = 0 # Variable for holding the first digit and the last digit 
+        end = len(line) - 1 # Get the size of string (line)
         i = 0
-        indexes = []
-        numbers = []
-        for i in range(0,len(digits)):
-            index = line.find(digits[i])
-            while(index > -1):
-                indexes.append(index)
-                numbers.append(digitsNumbers[i])
-                index = line.find(digits[i],index+1)
+        indexes = [] # List for holding indexes for all digits-strings
+        numbers = [] # List for holding values for all digits-strings
+        for i in range(0,len(digits)): # Go thru each pre-defined string
+            index = line.find(digits[i]) # Check if line contains the string
+            while(index > -1): # find() will output -1 if it won't find anything
+                indexes.append(index) # Append index of the string
+                numbers.append(digitsNumbers[i]) # Append Equivalent number for it
+                index = line.find(digits[i],index+1) # Check if line one more occurrence of the same string
         i = 0
-        while i < end:
+        while i < end: # Almost the same code from ex1
             if(line[i].isnumeric()):
-                indexes.append(i)
-                numbers.append(int(line[i]))
+                indexes.append(i) # Append index to compare it later with all indexes
+                numbers.append(int(line[i])) # Same for number
                 break
             i += 1
         i = end
@@ -32,11 +31,11 @@ for line in Lines:
                 break
             i -= 1
         print(f"index = {indexes}, numbers = {numbers}")
-        startIndexValue = indexes[0]
+        startIndexValue = indexes[0] # Initialize values
         startIndex = 0
         lastIndexValue = indexes[0]
         lastIndexValue = -1
-        for i in range(0,len(indexes)):
+        for i in range(0,len(indexes)): # Find the smallest and biggest index in index list
             if startIndexValue > indexes[i]:
                 startIndexValue = indexes[i]
                 startIndex = i
@@ -46,10 +45,10 @@ for line in Lines:
         
         print(f"The smallest index value : {startIndexValue} ({startIndex}) the biggest index: {lastIndexValue} ({lastIndex})")
         print(f"The smallest value: {numbers[startIndex]} ({startIndex}) the biggest value: {numbers[lastIndex]} ({lastIndex})")
-        endResult += numbers[startIndex] * 10 + numbers[lastIndex]
-        endoutput += endResult
+        endResult += numbers[startIndex] * 10 + numbers[lastIndex] # Merge the left and right numbers
+        endoutput += endResult # Add result to the final sum
         print(f"Sum: {endoutput} Num: {endResult}\n")
     except Exception as e:
         print(e)
         pass
-print(endoutput)
+print(f"Result: {endoutput}") # Print result
