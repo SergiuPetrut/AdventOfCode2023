e8ca35af6b06a214bf3a837d328a99392a874025
D3SXX <9647752d@gmail.com>
2023-12-04 20:12:53 +0200

    Add comments

--- day2/Python/Ex1.py
+++ day2/Python/Ex1.py
@@ -1,41 +1,41 @@
 
-cubes = ["blue", "red", "green"]
-maxCubesAmount = [14,12,13]
-file = open("game.txt","r")
-sumID = 0
-Lines = file.readlines()
+cubes = ["blue", "red", "green"] # List for pre-defined values for cubes
+maxCubesAmount = [14,12,13] # List for maximum allowed cubes in each set
+file = open("game.txt","r") # Open file in read-mode
+sumID = 0 # Final sum of all IDs
+Lines = file.readlines() # Read file by lines
 
-indexes = []
-names = []
-id = 0
-for i,line in enumerate(Lines):
-    cubesSum = [0,0,0]
-    check = [True,True,True]
+indexes = [] # Hold indexes for each cube in each set
+names = [] # Hold values for each cube in each set
+id = 0 # Initialize id
+for i,line in enumerate(Lines): # Going thru each line
+    cubesSum = [0,0,0] # Set initial sum for each cube to be 0
+    check = [True,True,True] # Set check for each bag
     try:
         for i in range(0,len(cubes)):
-            index = line.find(cubes[i])
-            while index > 0:
-                indexes.append(index)
-                names.append(cubes[i])
-                num_index = index - 2
+            index = line.find(cubes[i]) # Looking for cuber in cubes[3] order
+            while index > 0: # If the cube is present
+                indexes.append(index) # Record the index of the first mention 
+                names.append(cubes[i]) # Record the name of the cube for this index
+                num_index = index - 2 # Go back in line and look for the value
                 k = 1
                 value = 0
-                while line[num_index].isnumeric():
-                    value += int(line[num_index]) * k
-                    num_index -= 1
-                    k = k * 10
-                if value > maxCubesAmount[i]:
-                    check[i] = False
-                cubesSum[i] += value
-                index = line.find(cubes[i],index+1)
-        id += 1
+                while line[num_index].isnumeric(): # While the value is a number
+                    value += int(line[num_index]) * k # Get the value
+                    num_index -= 1 # Go one place back to see if is also a number
+                    k = k * 10 # Increase k for the next number
+                if value > maxCubesAmount[i]: # Check if the value is bigger than allowed
+                    check[i] = False # Mark false if is
+                cubesSum[i] += value # Record the value for cube to the total sum for this line
+                index = line.find(cubes[i],index+1) # Look for the next mention of cube color
+        id += 1 # Increase the ID by 1
         print(f"id: {id} - {cubes[0]}: {cubesSum[0]}, {cubes[1]}: {cubesSum[1]}, {cubes[2]}: {cubesSum[2]}")
 
-        passed = False
-        if all(i == True for i in check):
-            sumID += id
-            passed = True
+        passed = False # Initialize the variable for the final result for this line
+        if all(i == True for i in check): # Check if all numbers are in limit
+            sumID += id # Record ID of the possible game
+            passed = True # Set the variable to true
         print(f"id: {id} - {passed}; Sum is {sumID}\n")
     except Exception as e:
         print(e)
-print(f"Result: {sumID}")
+print(f"Result: {sumID}") # Print the final sum of all IDs where the game is possible
--- day2/Python/Ex2.py
+++ day2/Python/Ex2.py
@@ -1,38 +1,37 @@
 
-cubes = ["blue", "red", "green"]
-file = open("game.txt","r")
-powerSum = 0
-Lines = file.readlines()
+cubes = ["blue", "red", "green"] # List for pre-defined values for cubes
+file = open("game.txt","r") # Open file in read-mode
+powerSum = 0 # Final sum of all powers
+Lines = file.readlines() # Read file by lines
 
-indexes = []
-names = []
-id = 0
-for i,line in enumerate(Lines):
-    check = [True,True,True]
-    minimalCubesNeeded = [0,0,0]
+indexes = [] # Hold indexes for each cube in each set
+names = [] # Hold values for each cube in each set
+id = 0 # Initialize id
+for i,line in enumerate(Lines): # Going thru each line
+    minimalCubesNeeded = [0,0,0] # List for holding maximum values for cubes in a line
     try:
         for i in range(0,len(cubes)):
-            index = line.find(cubes[i])
-            while index > 0:
-                indexes.append(index)
-                names.append(cubes[i])
-                num_index = index - 2
+            index = line.find(cubes[i]) # Looking for cuber in cubes[3] order
+            while index > 0: # If the cube is present
+                indexes.append(index) # Record the index of the first mention 
+                names.append(cubes[i]) # Record the name of the cube for this index
+                num_index = index - 2 # Go back in line and look for the value
                 k = 1
                 value = 0
-                while line[num_index].isnumeric():
-                    value += int(line[num_index]) * k
-                    num_index -= 1
-                    k = k * 10
-                if(value > minimalCubesNeeded[i]):
-                    minimalCubesNeeded[i] = value
-                index = line.find(cubes[i],index+1)
-        id += 1
-        power = 1
-        for value in minimalCubesNeeded:
+                while line[num_index].isnumeric(): # While the value is a number
+                    value += int(line[num_index]) * k # Get the value
+                    num_index -= 1 # Go one place back to see if is also a number
+                    k = k * 10 # Increase k for the next number
+                if(value > minimalCubesNeeded[i]): # Check if the value is bigger than the one in previous set
+                    minimalCubesNeeded[i] = value # Record the value for cube if it is
+                index = line.find(cubes[i],index+1) # Look for the next mention of cube color
+        id += 1 # Increase the ID by 1
+        power = 1 # Initialize variable for power of all cubes
+        for value in minimalCubesNeeded: # Go thru each value in the list
             power = power * value
-        powerSum += power 
+        powerSum += power # Add power value of a line to the final result
         print(f"id: {id} - Minimal Cubes Needed: {cubes[0]}: {minimalCubesNeeded[0]}, {cubes[1]}: {minimalCubesNeeded[1]}, {cubes[2]}: {minimalCubesNeeded[2]}")
-        print(f"if: {id} - Power = {power}; Sum: {powerSum}\n")
+        print(f"if: {id} - Power: {power}, Sum: {powerSum}\n")
     except Exception as e:
         print(e)
-print(f"Result: {powerSum}")
+print(f"Result: {powerSum}") # Print the final sum of all powers
